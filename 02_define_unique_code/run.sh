e6ede73f0f09c30c2e53fdd54630107fd25408cf
Pete Bunting <petebunting@mac.com>
2020-07-14 16:19:43 +0100

    Update file name.

--- 02_define_unique_code/run.sh
+++ 02_define_unique_code/run.sh
@@ -3,6 +3,6 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif \
 python add_unqid_code.py --vecfile /scratch/a.pfb/gmw_calc_region_area_stats/data/GADM_EEZ_WCMC.gpkg --veclyr National \
 --unqcol gid_0 --unqidcol unqid --outvecfile /scratch/a.pfb/gmw_calc_region_area_stats/data/GADM_EEZ_WCMC_UnqID.gpkg \
---outveclyr National --format GPKG --lut /scratch/a.pfb/gmw_calc_region_area_stats/data/lut.json
+--outveclyr National --format GPKG --lut /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_lut.json
 
 
