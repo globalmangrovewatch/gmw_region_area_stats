ac05daaf539008b3d78800cb5a238afed1a21e4a
Pete Bunting <petebunting@mac.com>
2020-07-15 16:02:14 +0100

    remove excel output.

--- 06_summarise_tile_stats/run.sh
+++ 06_summarise_tile_stats/run.sh
@@ -4,9 +4,7 @@ singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a
 --outfile /scratch/a.pfb/gmw_calc_region_area_stats/stats/country_sub_stats/national_stats/gmw_2010_country_sub_stats.json \
 --lutfile /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_lut_sub.json \
 --csv /scratch/a.pfb/gmw_calc_region_area_stats/stats/country_sub_stats/national_stats/gmw_2010_country_sub_stats.csv \
---feather /scratch/a.pfb/gmw_calc_region_area_stats/stats/country_sub_stats/national_stats/gmw_2010_country_sub_stats.feather \
---excel /scratch/a.pfb/gmw_calc_region_area_stats/stats/country_sub_stats/national_stats/gmw_2010_country_sub_stats.xlsx 
-#--sheet sub2010 
+--feather /scratch/a.pfb/gmw_calc_region_area_stats/stats/country_sub_stats/national_stats/gmw_2010_country_sub_stats.feather
 
 
 
