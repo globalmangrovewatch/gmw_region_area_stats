ec9ea5d474996af407414ee82407ccca04b1ea13
Pete Bunting <petebunting@mac.com>
2020-07-15 10:25:25 +0100

    Add scripts to rasterise the tiles.

---
+++ 03_rasterise_rois/rasterise_roi.py
@@ -0,0 +1,32 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import rsgislib
+import rsgislib.imagecalc
+import rsgislib.vectorutils
+
+logger = logging.getLogger(__name__)
+
+class RasteriseStatsROIs(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='rasterise_roi.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        rsgislib.vectorutils.rasteriseVecLyr(self.params['roi_vec'], self.params['roi_vec_lyr'],
+                                             self.params['img_tile'], self.params['tile_roi_img'], gdalformat="KEA",
+                                             burnVal=1, datatype=rsgislib.TYPE_32UINT,
+                                             vecAtt=self.params['roi_vec_col'],
+                                             vecExt=False, thematic=True, nodata=0)
+
+    def required_fields(self, **kwargs):
+        return ["img_tile", "roi_vec", "roi_vec_lyr", "roi_vec_col", "tile_roi_img"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['tile_roi_img']] = 'gdal_image'
+        return self.check_files(files_dict)
+
+if __name__ == "__main__":
+    RasteriseStatsROIs().std_run()
+
+
