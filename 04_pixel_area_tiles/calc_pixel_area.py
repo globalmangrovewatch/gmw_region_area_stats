105de10bf81eec4549b4c21ff582d262044d5e79
Pete Bunting <petebunting@mac.com>
2020-07-15 11:24:48 +0100

    Added scripts to calculate the pixel areas.

---
+++ 04_pixel_area_tiles/calc_pixel_area.py
@@ -0,0 +1,29 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import rsgislib
+import rsgislib.imagecalc
+import rsgislib.vectorutils
+
+logger = logging.getLogger(__name__)
+
+class CalcPixelArea(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='calc_pixel_area.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        rsgislib.imagecalc.calcWSG84PixelArea(self.params['img_tile'], self.params['tile_pxa_img'],
+                                              scale=10000, gdalformat='KEA')
+
+    def required_fields(self, **kwargs):
+        return ["img_tile", "tile_pxa_img"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['tile_pxa_img']] = 'gdal_image'
+        return self.check_files(files_dict)
+
+if __name__ == "__main__":
+    CalcPixelArea().std_run()
+
+
---
+++ 04_pixel_area_tiles/gen_pixel_area_cmds.py
@@ -0,0 +1,49 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import rsgislib
+import logging
+import os
+import glob
+import geopandas
+
+logger = logging.getLogger(__name__)
+
+class GenTilePixAreaCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        rsgis_utils = rsgislib.RSGISPyUtils()
+        img_tiles = glob.glob(kwargs['img_tiles'])
+        for img_tile in img_tiles:
+            tile_base_name = rsgis_utils.get_file_basename(img_tile, checkvalid=False)
+            tile_base_name = tile_base_name.replace(kwargs['tile_name_rm'], '')
+            out_pxa_file = os.path.join(kwargs['out_pxa_path'], "{}_pxa.kea".format(tile_base_name))
+            c_dict = dict()
+            c_dict['img_tile'] = img_tile
+            c_dict['tile_pxa_img'] = out_pxa_file
+            self.params.append(c_dict)
+
+    def run_gen_commands(self):
+        self.gen_command_info(img_tiles='/scratch/a.pfb/gmw_calc_region_area_stats/data/gmw_tiles_v2/gmw2010v2.0/*.tif',
+                              tile_name_rm='_gmw2010v2.0',
+                              out_pxa_path='/scratch/a.pfb/gmw_calc_region_area_stats/data/tile_stats/2010')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("gmw_tiles_pxa", 8224, '/scratch/a.pfb/gmw_calc_region_area_stats/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+    def run_check_outputs(self):
+        process_tools_mod = 'calc_pixel_area'
+        process_tools_cls = 'CalcPixelArea'
+        time_sample_str = self.generate_readable_timestamp_str()
+        out_err_file = 'processing_errs_{}.txt'.format(time_sample_str)
+        out_non_comp_file = 'non_complete_errs_{}.txt'.format(time_sample_str)
+        self.check_job_outputs(process_tools_mod, process_tools_cls, out_err_file, out_non_comp_file)
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("calc_pixel_area.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    create_tools = GenTilePixAreaCmds(cmd=script_cmd, sqlite_db_file="gmw_tile_pix_area.db")
+    create_tools.parse_cmds()
