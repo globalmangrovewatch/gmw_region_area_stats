6bbb3fa1f8a97a8574c5f9d6464aec625350a2f9
Pete Bunting <petebunting@mac.com>
2020-07-15 11:57:32 +0100

    Move directory for stats calculation and now find existing pre-calculated tile files.

--- 05_calc_tiles_stats/calc_tile_gmw_extents.py
+++ 05_calc_tiles_stats/calc_tile_gmw_extents.py
@@ -1,10 +1,7 @@
 from pbprocesstools.pbpt_q_process import PBPTQProcessTool
 import logging
 import os
-import shutil
 import rsgislib
-import rsgislib.imagecalc
-import rsgislib.vectorutils
 import numpy
 import osgeo.gdal as gdal
 
@@ -47,37 +44,24 @@ class CalcTileGMWExtent(PBPTQProcessTool):
         super().__init__(cmd_name='gen_calc_tile_extent_cmds.py', descript=None)
 
     def do_processing(self, **kwargs):
-        rsgis_utils = rsgislib.RSGISPyUtils()
-        tile_base_name = rsgis_utils.get_file_basename(self.params['img_tile'], checkvalid=True)
-        pix_area_img = os.path.join(self.params['tmp_dir'], "{}_pix_area.kea".format(tile_base_name))
-        rsgislib.imagecalc.calcWSG84PixelArea(self.params['img_tile'], pix_area_img, scale=10000, gdalformat='KEA')
-
-        uid_img = os.path.join(self.params['tmp_dir'], "{}_uid.kea".format(tile_base_name))
-        rsgislib.vectorutils.rasteriseVecLyr(self.params['roi_vec'], self.params['roi_vec_lyr'],
-                                             self.params['img_tile'], uid_img, gdalformat="KEA",
-                                             burnVal=1, datatype=rsgislib.TYPE_16UINT,
-                                             vecAtt=self.params['roi_vec_col'],
-                                             vecExt=False, thematic=True, nodata=0)
-
         lut_vals = dict()
         for val in self.params['unq_vals']:
             lut_vals[val] = dict()
             lut_vals[val]['count'] = 0
             lut_vals[val]['area'] = 0.0
 
-        calc_unq_val_pxl_areas(pix_area_img, uid_img, lut_vals)
+        calc_unq_val_pxl_areas(self.params['tile_pxa_img'], self.params['tile_roi_img'], lut_vals)
 
         for val in self.params['unq_vals']:
             lut_vals[val]['count'] = int(lut_vals[val]['count'])
             lut_vals[val]['area'] = float(lut_vals[val]['area'])
 
+        rsgis_utils = rsgislib.RSGISPyUtils()
         rsgis_utils.writeDict2JSON(lut_vals, self.params['out_file'])
 
-        if os.path.exists(self.params['tmp_dir']):
-            shutil.rmtree(self.params['tmp_dir'])
 
     def required_fields(self, **kwargs):
-        return ["img_tile", "unq_vals", "roi_vec", "roi_vec_lyr", "roi_vec_col", "out_file", "tmp_dir"]
+        return ["img_tile", "unq_vals", "tile_pxa_img", "tile_roi_img", "out_file",]
 
     def outputs_present(self, **kwargs):
         return os.path.exists(self.params['out_file']), dict()
---
+++ 05_calc_tiles_stats/gen_calc_tile_extent_cmds.py
@@ -0,0 +1,70 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import rsgislib
+import logging
+import os
+import glob
+import geopandas
+
+logger = logging.getLogger(__name__)
+
+class GenTileExtentCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        base_gpdf = geopandas.read_file(kwargs['roi_vec'], layer=kwargs['roi_vec_lyr'])
+        unq_vals = base_gpdf[kwargs['roi_vec_col']].unique().tolist() 
+        base_gpdf = None
+
+        rsgis_utils = rsgislib.RSGISPyUtils()
+        img_tiles = glob.glob(kwargs['img_tiles'])
+        for img_tile in img_tiles:
+            tile_base_name = rsgis_utils.get_file_basename(img_tile, checkvalid=False)
+            tile_generic_base_name = tile_base_name.replace(kwargs['tile_name_rm'], '')
+            tile_roi_img = os.path.join(kwargs['roi_img_path'], "{}_roi.kea".format(tile_generic_base_name))
+            if not os.path.exists(tile_roi_img):
+                raise Exception("Could not file ROI image file: {}".format(tile_roi_img))
+            tile_pxa_img = os.path.join(kwargs['pxa_img_path'], "{}_pxa.kea".format(tile_generic_base_name))
+            if not os.path.exists(tile_pxa_img):
+                raise Exception("Could not file Pixel Area image file: {}".format(tile_pxa_img))
+            out_file = os.path.join(kwargs['out_path'], "{}_stats.json".format(tile_base_name))
+            c_dict = dict()
+            c_dict['img_tile'] = img_tile
+            c_dict['unq_vals'] = unq_vals
+            c_dict['tile_pxa_img'] = tile_pxa_img
+            c_dict['tile_roi_img'] = tile_roi_img
+            c_dict['out_file'] = out_file
+            self.params.append(c_dict)
+
+    def run_gen_commands(self):
+
+        self.gen_command_info(img_tiles='/scratch/a.pfb/gmw_calc_region_area_stats/data/gmw_tiles_v2/gmw2010v2.0/*.tif',
+                              tile_name_rm='_gmw2010v2.0',
+                              roi_vec='/scratch/a.pfb/gmw_calc_region_area_stats/data/GADM_EEZ_WCMC_UnqID.gpkg',
+                              roi_vec_lyr='National',
+                              roi_vec_col='unqid',
+                              pxa_img_path='/scratch/a.pfb/gmw_calc_region_area_stats/data/pixel_area_tiles',
+                              roi_img_path='/scratch/a.pfb/gmw_calc_region_area_stats/data/roi_tiles/country_sub_roi_tiles',
+                              out_path='/scratch/a.pfb/gmw_calc_region_area_stats/stats/tile_stats/2010',
+                              tmp_dir='/scratch/a.pfb/gmw_calc_region_area_stats/tmp')
+
+        self.pop_params_db()
+        self.create_slurm_sub_sh("gmw_tiles_stats", 8224, '/scratch/a.pfb/gmw_calc_region_area_stats/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+    def run_check_outputs(self):
+        process_tools_mod = 'calc_tile_gmw_extents'
+        process_tools_cls = 'CalcTileGMWExtent'
+        time_sample_str = self.generate_readable_timestamp_str()
+        out_err_file = 'processing_errs_{}.txt'.format(time_sample_str)
+        out_non_comp_file = 'non_complete_errs_{}.txt'.format(time_sample_str)
+        self.check_job_outputs(process_tools_mod, process_tools_cls, out_err_file, out_non_comp_file)
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("calc_tile_gmw_extents.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    create_tools = GenTileExtentCmds(cmd=script_cmd, sqlite_db_file="gmw_tile_stats.db")
+    create_tools.parse_cmds()
