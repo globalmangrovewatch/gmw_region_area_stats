f3e7cc3e7a119ac6c8e44ffeafd2aa3e42b4fce6
Pete Bunting <petebunting@mac.com>
2020-07-16 09:41:08 +0100

    add script to summarise wdpa ramsar sites.

---
+++ 06_summarise_tile_stats/run_wdpa_ramsar_stats.sh
@@ -0,0 +1,57 @@
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
+/scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_tile_statistics.py \
+--indir /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/tile_stats/1996 \
+--outfile /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_1996_wdpa_ramsar_stats.json \
+--lutfile /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_wdpa_lut.json \
+--csv /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_1996_wdpa_ramsar_stats.csv \
+--feather /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_1996_wdpa_ramsar_stats.feather
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
+/scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_tile_statistics.py \
+--indir /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/tile_stats/2007 \
+--outfile /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2007_wdpa_ramsar_stats.json \
+--lutfile /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_wdpa_lut.json \
+--csv /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2007_wdpa_ramsar_stats.csv \
+--feather /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2007_wdpa_ramsar_stats.feather
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
+/scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_tile_statistics.py \
+--indir /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/tile_stats/2008 \
+--outfile /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2008_wdpa_ramsar_stats.json \
+--lutfile /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_wdpa_lut.json \
+--csv /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2008_wdpa_ramsar_stats.csv \
+--feather /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2008_wdpa_ramsar_stats.feather
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
+/scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_tile_statistics.py \
+--indir /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/tile_stats/2009 \
+--outfile /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2009_wdpa_ramsar_stats.json \
+--lutfile /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_wdpa_lut.json \
+--csv /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2009_wdpa_ramsar_stats.csv \
+--feather /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2009_wdpa_ramsar_stats.feather
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
+/scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_tile_statistics.py \
+--indir /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/tile_stats/2010 \
+--outfile /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2010_wdpa_ramsar_stats.json \
+--lutfile /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_wdpa_lut.json \
+--csv /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2010_wdpa_ramsar_stats.csv \
+--feather /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2010_wdpa_ramsar_stats.feather
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
+/scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_tile_statistics.py \
+--indir /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/tile_stats/2015 \
+--outfile /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2015_wdpa_ramsar_stats.json \
+--lutfile /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_wdpa_lut.json \
+--csv /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2015_wdpa_ramsar_stats.csv \
+--feather /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2015_wdpa_ramsar_stats.feather
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
+/scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_tile_statistics.py \
+--indir /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/tile_stats/2016 \
+--outfile /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2016_wdpa_ramsar_stats.json \
+--lutfile /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_wdpa_lut.json \
+--csv /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2016_wdpa_ramsar_stats.csv \
+--feather /scratch/a.pfb/gmw_calc_region_area_stats/stats/wdpa_ramsar_stats/national_stats/gmw_2016_wdpa_ramsar_stats.feather
+
+
