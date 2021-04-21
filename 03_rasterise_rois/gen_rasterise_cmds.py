be1afd496cbd2180f068d4c0dda5d3722c4c129b
Pete Bunting <petebunting@mac.com>
2021-04-21 21:59:42 +0100

    Update to work with newer version of pbprocesstools and calculate stats for GMW v3.

--- 03_rasterise_rois/gen_rasterise_cmds.py
+++ 03_rasterise_rois/gen_rasterise_cmds.py
@@ -14,7 +14,7 @@ class GenTileExtentCmds(PBPTGenQProcessToolCmds):
         for img_tile in img_tiles:
             tile_base_name = rsgis_utils.get_file_basename(img_tile, checkvalid=False)
             tile_base_name = tile_base_name.replace(kwargs['tile_name_rm'], '')
-            out_roi_file = os.path.join(kwargs['out_roi_path'], "{}_roi.kea".format(tile_base_name))
+            out_roi_file = os.path.join(kwargs['out_roi_path'], "{}_roi_{}.kea".format(tile_base_name, kwargs['roi_name']))
             if not os.path.exists(out_roi_file):
                 c_dict = dict()
                 c_dict['img_tile'] = img_tile
@@ -25,22 +25,25 @@ class GenTileExtentCmds(PBPTGenQProcessToolCmds):
                 self.params.append(c_dict)
 
     def run_gen_commands(self):
-        self.gen_command_info(img_tiles='/scratch/a.pfb/gmw_calc_region_area_stats/data/gmw_tiles_v2/gmw2010v2.0/*.tif',
-                              tile_name_rm='_gmw2010v2.0',
+        self.gen_command_info(img_tiles='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_qa/*.kea',
+                              tile_name_rm='_tile_gmw_v3_init_qad',
+                              roi_name='countries_sub',
                               roi_vec='/scratch/a.pfb/gmw_calc_region_area_stats/data/GADM_EEZ_WCMC_subset_UnqID.gpkg',
                               roi_vec_lyr='National',
                               roi_vec_col='unqid',
                               out_roi_path='/scratch/a.pfb/gmw_calc_region_area_stats/data/roi_tiles/country_sub_roi_tiles')
 
-        self.gen_command_info(img_tiles='/scratch/a.pfb/gmw_calc_region_area_stats/data/gmw_tiles_v2/gmw2010v2.0/*.tif',
-                              tile_name_rm='_gmw2010v2.0',
+        self.gen_command_info(img_tiles='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_qa/*.kea',
+                              tile_name_rm='_tile_gmw_v3_init_qad',
+                              roi_name='countries',
                               roi_vec='/scratch/a.pfb/gmw_calc_region_area_stats/data/GADM_EEZ_WCMC_UnqID.gpkg',
                               roi_vec_lyr='National',
                               roi_vec_col='unqid',
                               out_roi_path='/scratch/a.pfb/gmw_calc_region_area_stats/data/roi_tiles/country_roi_tiles')
 
-        self.gen_command_info(img_tiles='/scratch/a.pfb/gmw_calc_region_area_stats/data/gmw_tiles_v2/gmw2010v2.0/*.tif',
-                              tile_name_rm='_gmw2010v2.0',
+        self.gen_command_info(img_tiles='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_qa/*.kea',
+                              tile_name_rm='_tile_gmw_v3_init_qad',
+                              roi_name='ramsar',
                               roi_vec='/scratch/a.pfb/gmw_calc_region_area_stats/data/wdpa_july2020_regions_ramsar_UnqID.gpkg',
                               roi_vec_lyr='polys',
                               roi_vec_col='unqid',
@@ -53,23 +56,15 @@ class GenTileExtentCmds(PBPTGenQProcessToolCmds):
                                  job_time_limit='2-23:59',
                                  module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
 
-    def run_check_outputs(self):
-        process_tools_mod = 'rasterise_roi'
-        process_tools_cls = 'RasteriseStatsROIs'
-        time_sample_str = self.generate_readable_timestamp_str()
-        out_err_file = 'processing_errs_{}.txt'.format(time_sample_str)
-        out_non_comp_file = 'non_complete_errs_{}.txt'.format(time_sample_str)
-        self.check_job_outputs(process_tools_mod, process_tools_cls, out_err_file, out_non_comp_file)
-
-    def run_remove_outputs(self, all_jobs=False, error_jobs=False):
-        process_tools_mod = 'rasterise_roi'
-        process_tools_cls = 'RasteriseStatsROIs'
-        self.remove_job_outputs(process_tools_mod, process_tools_cls, all_jobs, error_jobs)
-
 
 if __name__ == "__main__":
     py_script = os.path.abspath("rasterise_roi.py")
     script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
 
-    create_tools = GenTileExtentCmds(cmd=script_cmd, sqlite_db_file="gmw_rasterise_roi.db")
+    process_tools_mod = 'rasterise_roi'
+    process_tools_cls = 'RasteriseStatsROIs'
+
+    create_tools = GenTileExtentCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_stats_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
     create_tools.parse_cmds()
