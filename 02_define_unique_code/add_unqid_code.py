cb1db9562827c3d0f0fff67ccb0f9a39a51600f3
Pete Bunting <petebunting@mac.com>
2022-11-25 20:59:32 +0000

    Update.

--- 02_define_unique_code/add_unqid_code.py
+++ 02_define_unique_code/add_unqid_code.py
@@ -1,64 +1,67 @@
+import numpy
 
-
-def add_unq_id_val(vec_file, vec_lyr, unq_col, unq_id_col, out_vec_file, out_vec_lyr, out_format="GPKG", lut_json_file=None):
+def add_unq_numeric_col(
+    vec_file: str,
+    vec_lyr: str,
+    unq_col: str,
+    out_col: str,
+    out_vec_file: str,
+    out_vec_lyr: str,
+    out_format: str = "GPKG",
+    lut_json_file: str = None
+):
     """
-    A function which splits a vector layer by an attribute value into either different layers or different output
-    files.
+    A function which adds a numeric column based off an existing column in
+    the vector file.
 
-    :param vec_file: Input vector file
-    :param vec_lyr: Input vector layer
-    :param unq_col: The column name used to define unique regions.
-    :param unq_id_col: The output column name used to create a numeric unique ID
-    :param out_vec_file: The output vector file with the selected features.
-    :param out_vec_lyr: The output layer file with the selected features.
-    :param out_format: the output vector format (Default: GPKG)
-    :param lut_json_file: An output file with the LUT linking numeric ID and region unique value.
-                          If None no output is produced.
+    :param vec_file: Input vector file.
+    :param vec_lyr: Input vector layer within the input file.
+    :param unq_col: The column within which the unique values will be identified.
+    :param out_col: The output numeric column
+    :param out_vec_file: Output vector file
+    :param out_vec_lyr: output vector layer name.
+    :param out_format: output file format (default GPKG).
+    :param lut_json_file: an optional output LUT file.
 
     """
     import geopandas
-    import numpy
-    import tqdm
+    import rsgislib.vectorutils
+    import rsgislib.tools.utils
+
+    out_format = rsgislib.vectorutils.check_format_name(out_format)
 
     base_gpdf = geopandas.read_file(vec_file, layer=vec_lyr)
     unq_vals = base_gpdf[unq_col].unique()
-    base_gpdf[unq_id_col] = numpy.zeros((base_gpdf.shape[0]), dtype=int)
 
-    unq_id_val = 1
+    base_gpdf[out_col] = numpy.zeros((base_gpdf.shape[0]), dtype=int)
     lut = dict()
     lut['id'] = dict()
     lut['val'] = dict()
-    for unq_val in tqdm.tqdm(unq_vals):
-        base_gpdf.loc[base_gpdf[unq_col] == unq_val, unq_id_col] = unq_id_val
-        lut['id'][unq_id_val] = unq_val
-        lut['val'][unq_val] = unq_id_val
-        unq_id_val += 1
+    num_unq_val = 1
+    for unq_val in unq_vals:
+        sel_rows = base_gpdf[unq_col] == unq_val
+        base_gpdf.loc[sel_rows, out_col] = num_unq_val
+        lut['id'][num_unq_val] = unq_val
+        lut['val'][unq_val] = num_unq_val
+        num_unq_val += 1
 
-    if out_format == 'GPKG':
+    if out_format == "GPKG":
         base_gpdf.to_file(out_vec_file, layer=out_vec_lyr, driver=out_format)
     else:
         base_gpdf.to_file(out_vec_file, driver=out_format)
 
     if lut_json_file is not None:
-        import rsgislib
-        rsgis_utils = rsgislib.RSGISPyUtils()
-        rsgis_utils.writeDict2JSON(lut, lut_json_file)
+        rsgislib.tools.utils.write_dict_to_json(lut, lut_json_file)
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--vecfile", type=str, required=True, help="Input File.")
-    parser.add_argument("--veclyr", type=str, required=True, help="Input vector layer.")
-    parser.add_argument("--unqcol", type=str, required=True, help="Unique Values Column.")
-    parser.add_argument("--unqidcol", type=str, required=True, help="Unique ID column.")
-    parser.add_argument("--outvecfile", type=str, required=True, help="Output File.")
-    parser.add_argument("--outveclyr", type=str, required=True, help="Output vector layer.")
-    parser.add_argument("--format", type=str, required=True, help="Output vector format.")
-    parser.add_argument("--lut", type=str, required=False, help="Output LUT JSON file.")
+vec_file = "/home/pete/Documents/gmw_v3_regional_stats/data/UNBoundaries_wEEZ.gpkg"
+vec_lyr = "UNBoundaries_wEEZ"
 
-    args = parser.parse_args()
-    add_unq_id_val(args.vecfile, args.veclyr, args.unqcol, args.unqidcol, args.outvecfile,
-                   args.outveclyr, args.format, args.lut)
+out_vec_file = "/home/pete/Documents/gmw_v3_regional_stats/data/UNBoundaries_wEEZ_unq.gpkg"
+out_vec_lyr = "UNBoundaries_wEEZ_unq"
+
+lut_json_file = "un_boundaries_lut.json"
+
+add_unq_numeric_col(vec_file, vec_lyr, "ISO3CD", "cntry_uid", out_vec_file, out_vec_lyr, "GPKG", lut_json_file)
 
 
 
--- 02_define_unique_code/run.sh
+++ 02_define_unique_code/run.sh
@@ -5,14 +5,3 @@ python add_unqid_code.py --vecfile /scratch/a.pfb/gmw_calc_region_area_stats/dat
 --unqcol gid_0 --unqidcol unqid --outvecfile /scratch/a.pfb/gmw_calc_region_area_stats/data/GADM_EEZ_WCMC_UnqID.gpkg \
 --outveclyr National --format GPKG --lut /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_lut.json
 
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif \
-python add_unqid_code.py --vecfile /scratch/a.pfb/gmw_calc_region_area_stats/data/GADM_EEZ_WCMC_subset.gpkg --veclyr National \
---unqcol gid_0 --unqidcol unqid --outvecfile /scratch/a.pfb/gmw_calc_region_area_stats/data/GADM_EEZ_WCMC_subset_UnqID.gpkg \
---outveclyr National --format GPKG --lut /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_lut_sub.json
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif \
-python add_unqid_code.py --vecfile /scratch/a.pfb/gmw_calc_region_area_stats/data/wdpa_july2020_regions_ramsar.gpkg --veclyr polys \
---unqcol NAME --unqidcol unqid --outvecfile /scratch/a.pfb/gmw_calc_region_area_stats/data/wdpa_july2020_regions_ramsar_UnqID.gpkg \
---outveclyr polys --format GPKG --lut /scratch/a.pfb/gmw_calc_region_area_stats/data/unq_id_wdpa_lut.json
-
-
