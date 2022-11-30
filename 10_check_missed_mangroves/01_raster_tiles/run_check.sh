cfb8902e8e52ce6452c36674bc9ce0402cd2abfd
Pete Bunting <petebunting@mac.com>
2022-11-30 21:23:59 +0000

    Add script for checking image outputs.

---
+++ 10_check_missed_mangroves/01_raster_tiles/run_check.sh
@@ -0,0 +1,12 @@
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_1996_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2007_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2008_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2009_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2010_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2015_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2016_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2017_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2018_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2019_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_regional_stats/data/tests/tile_rasters/gmw_2020_v3/*.kea" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+
