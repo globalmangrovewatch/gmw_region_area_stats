a3775d033d6f942c6d82c55420dff6fc8b8a1c7e
Pete Bunting <petebunting@mac.com>
2023-03-07 21:52:32 +0000

    Update file names.

--- 09_correct_mangrove_areas/calc_corrected_areas.py
+++ 09_correct_mangrove_areas/calc_corrected_areas.py
@@ -1,7 +1,7 @@
 import pandas
 
-extent_file = "00_base_stats/gmw_v3_m49_un1_stats.feather"
-chngs_file = "00_base_stats/gmw_v3_chng_f1996_m49_un1_stats.feather"
+extent_file = "00_base_stats/gmw_v3_gmw_fid_uid_stats.feather"
+chngs_file = "00_base_stats/gmw_v3_chng_f1996_gmw_fid_uid_stats.feather"
 
 extent_stats_df = pandas.read_feather(extent_file)
 chng_stats_df = pandas.read_feather(chngs_file)
@@ -35,11 +35,11 @@ chng_stats_df = chng_stats_df.drop(columns=cols_rm)
 chng_stats_df = chng_stats_df.drop(columns=["index"])
 
 # Calculate the global totals and print for info
-extent_totals_df = extent_stats_df.drop(columns=["m49_un1"]).sum(axis=0)
+extent_totals_df = extent_stats_df.drop(columns=["gmw_fid_uid"]).sum(axis=0)
 print(extent_totals_df/100)
 
 # Create seperate gain and loss dataframes
-chng_totals_df = chng_stats_df.drop(columns=["m49_un1"]).sum(axis=0)
+chng_totals_df = chng_stats_df.drop(columns=["gmw_fid_uid"]).sum(axis=0)
 
 gain_cols = ["2007_area_gain", "2008_area_gain", "2009_area_gain", "2010_area_gain", "2015_area_gain", "2016_area_gain", "2017_area_gain", "2018_area_gain", "2019_area_gain", "2020_area_gain"]
 loss_cols = ["2007_area_loss", "2008_area_loss", "2009_area_loss", "2010_area_loss", "2015_area_loss", "2016_area_loss", "2017_area_loss", "2018_area_loss", "2019_area_loss", "2020_area_loss"]
@@ -90,21 +90,21 @@ chng_loss_corr_stats_df[loss_cols] = chng_loss_corr_stats_df[loss_cols] + (chng_
 
 
 # Export corrected gain and loss files
-chng_gain_corr_stats_df.to_csv("gmw_v3_chng_gain_corrected_m49_un1.csv")
-chng_loss_corr_stats_df.to_csv("gmw_v3_chng_loss_corrected_m49_un1.csv")
+chng_gain_corr_stats_df.to_csv("gmw_v3_chng_gain_corrected_gmw_fid_uid.csv")
+chng_loss_corr_stats_df.to_csv("gmw_v3_chng_loss_corrected_gmw_fid_uid.csv")
 
-chng_gain_corr_stats_df.to_feather("gmw_v3_chng_gain_corrected_m49_un1.feather")
-chng_loss_corr_stats_df.to_feather("gmw_v3_chng_loss_corrected_m49_un1.feather")
+chng_gain_corr_stats_df.to_feather("gmw_v3_chng_gain_corrected_gmw_fid_uid.feather")
+chng_loss_corr_stats_df.to_feather("gmw_v3_chng_loss_corrected_gmw_fid_uid.feather")
 
-chng_gain_corr_stats_df.to_excel("gmw_v3_chng_gain_corrected_m49_un1.xlsx")
-chng_loss_corr_stats_df.to_excel("gmw_v3_chng_loss_corrected_m49_un1.xlsx")
+chng_gain_corr_stats_df.to_excel("gmw_v3_chng_gain_corrected_gmw_fid_uid.xlsx")
+chng_loss_corr_stats_df.to_excel("gmw_v3_chng_loss_corrected_gmw_fid_uid.xlsx")
 
 
 
 # Define region as the index so all countries are correctly matched.
-chng_gain_corr_stats_df = chng_gain_corr_stats_df.set_index("m49_un1")
-chng_loss_corr_stats_df = chng_loss_corr_stats_df.set_index("m49_un1")
-extent_stats_corr_df = extent_stats_corr_df.set_index("m49_un1")
+chng_gain_corr_stats_df = chng_gain_corr_stats_df.set_index("gmw_fid_uid")
+chng_loss_corr_stats_df = chng_loss_corr_stats_df.set_index("gmw_fid_uid")
+extent_stats_corr_df = extent_stats_corr_df.set_index("gmw_fid_uid")
 
 # Calculate the corrected values per year based on change and 1996 area
 gmw_chng_years = [2007, 2008, 2009, 2010, 2015, 2016, 2017, 2018, 2019, 2020]
@@ -121,6 +121,6 @@ for i, row in no_chng_rgns_df.iterrows():
 
 # Export Corrected Country Areas
 extent_stats_corr_df = extent_stats_corr_df.reset_index()
-extent_stats_corr_df.to_csv("gmw_v3_m49_un1_areas_corrected.csv")
-extent_stats_corr_df.to_feather("gmw_v3_m49_un1_areas_corrected.feather")
-extent_stats_corr_df.to_excel("gmw_v3_m49_un1_areas_corrected.xlsx")
+extent_stats_corr_df.to_csv("gmw_v3_gmw_fid_uid_areas_corrected.csv")
+extent_stats_corr_df.to_feather("gmw_v3_gmw_fid_uid_areas_corrected.feather")
+extent_stats_corr_df.to_excel("gmw_v3_gmw_fid_uid_areas_corrected.xlsx")
